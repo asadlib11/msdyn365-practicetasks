c583932ebcd88a1cd2dbc4c3415007910fb3a0f3
Asad Ali Baloch <asadalibaloch059@gmail.com>
2020-03-18 16:08:43 +0500

    page-data-action load

--- src/modules/my-featured-content/my-featured-content.data.ts
+++ src/modules/my-featured-content/my-featured-content.data.ts
@@ -1,3 +1,5 @@
+import { AsyncResult, SimpleProduct } from '@msdyn365-commerce/retail-proxy';
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
@@ -5,4 +7,5 @@
 
 export interface IMyFeaturedContentData {
     actionResponse: {text: string};
+    products: AsyncResult<SimpleProduct>[];
 }
